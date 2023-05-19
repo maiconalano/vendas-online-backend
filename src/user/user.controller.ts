566d3034b6604b8c05db110ba74c0c70e2ac7436
maiconalano <maicon.alano@outlook.com>
2023-05-19 10:40:24 -0300

    feat: services

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,14 +1,19 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
-import { ObjectUnsubscribedError } from 'rxjs';
+import { UserService } from './user.service';
+import { User } from './interface/user.interface';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
+
   @Post()
-  async createUser(@Body() createUser: CreateUserDto) {
-    return {
-      ...createUser,
-      password: undefined,
-    };
+  async createUser(@Body() createUser: CreateUserDto): Promise<User> {
+    return this.userService.createUser(createUser);
+  }
+
+  @Get()
+  async getAllUser(): Promise<User[]> {
+    return this.userService.getAllUser();
   }
 }
