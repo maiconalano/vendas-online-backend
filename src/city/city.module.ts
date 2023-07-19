37bd56cbdea89bcc04045fedc3ab3c10edf34d21
maiconalano <maicon.alano@outlook.com>
2023-07-19 11:14:37 -0300

    feat: create city controller and insert cache

--- src/city/city.module.ts
+++ src/city/city.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
 import { CityController } from './city.controller';
 import { CityService } from './city.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CityEntity } from './entities/city.entity';
+import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
+  imports: [
+    CacheModule.register({
+      ttl: 9000000,
+    }),
+    TypeOrmModule.forFeature([CityEntity]),
+  ],
   controllers: [CityController],
-  providers: [CityService]
+  providers: [CityService],
 })
 export class CityModule {}
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,20 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { InjectRepository } from '@nestjs/typeorm/dist/common';
 import { Repository } from 'typeorm';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
 
 @Injectable()
 export class CityService {
   constructor(
     @InjectRepository(CityEntity)
     private readonly cityRepository: Repository<CityEntity>,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
-    return this.cityRepository.find({
+    const citiesCache: CityEntity[] = await this.cacheManager.get(
+      `state_${stateId}`,
+    );
+
+    if (citiesCache) {
+      return citiesCache;
+    }
+
+    const cities = await this.cityRepository.find({
       where: {
         stateId,
       },
     });
+
+    await this.cacheManager.set(`state_${stateId}`, cities);
+
+    return cities;
   }
 }
